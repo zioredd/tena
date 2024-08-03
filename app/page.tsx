19a6c923bba67cd9479fd0ac9f8c15cdb8b938e6
kaleab213 <kaleabtibebu400@gmail.com>
2024-08-03 12:29:32 +0300

    initial setup

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-3xl underline">Home</h1>
+      <Button>Sign Up</Button>
+    </div>
+  );
+}
